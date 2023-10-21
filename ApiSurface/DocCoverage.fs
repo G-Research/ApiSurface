a6c2e68ca9ee629a48cc535f92e1fb73e6fca5e2
Smaug123 <patrick+github@patrickstevens.co.uk>
2023-10-21 16:49:10 +0100

    Actual fix for compiler-generated types

--- ApiSurface/DocCoverage.fs
+++ ApiSurface/DocCoverage.fs
@@ -99,10 +99,12 @@ module DocCoverage =
         let types =
             assembly.GetTypes ()
             |> Array.filter (fun ty ->
-                // Skip the new code-analysis types added in net7 (discussion ongoing: see
-                // https://github.com/dotnet/runtime/issues/93699 )
+                // A version of the F# compiler in 7.0.400 emitted some generated types which were not marked
+                // as compiler-generated. Skip those.
+                // https://github.com/dotnet/fsharp/issues/16141
                 not (ty.FullName.StartsWith ("System.Diagnostics.CodeAnalysis.Dynamic", StringComparison.Ordinal))
             )
+            |> Array.filter (fun ty -> obj.ReferenceEquals (ty.GetCustomAttribute<CompilerGeneratedAttribute> (), null))
 
         let getSourceConstructFlags (compilationMapping : CompilationMappingAttribute) =
             if obj.ReferenceEquals (compilationMapping, null) then
